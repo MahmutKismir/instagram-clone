117eefec2a88c493c7f4f98b0de633ff3d06217b
Mahmut Kismir <103948581+MahmutKismir@users.noreply.github.com>
2023-04-14 04:57:09 +0300

    discover-explore page and modal edits

--- src/pages/UserHomeLayout/LefMenu/LeftMenuSections/UserHome/topImageSlider/data.js
+++ src/pages/UserHomeLayout/LefMenu/LeftMenuSections/UserHome/topImageSlider/data.js
@@ -1,4 +1,5 @@
 import mjdunk from '../../../../../../static/mjdunk.mp4'
+import vincereels from '../../../../../../static/vincereel.mp4'
 export const reelsData = [
     {
       id: 1,
@@ -11,8 +12,7 @@ export const reelsData = [
       id: 2,
       username: "airCanada",
       name: "Vince Carter",
-      image:
-        "https://golfdigest.sports.sndimg.com/content/dam/images/golfdigest/fullset/2020/06/vc3.jpg.rend.hgtvcom.1280.914.suffix/1593191957820.jpeg",
+      reels:vincereels        
     },
     {
       id: 3,
@@ -22,156 +22,83 @@ export const reelsData = [
         "https://media.gettyimages.com/id/51188472/photo/tracy-mcgrady-portraits.jpg?s=1024x1024&w=gi&k=20&c=V6RLb1Q3ZZrFGljovDMhhwJDcR1i_f_uDwqoZJ3x-6M=",
     },
     {
-      id: 12,
+      id: 4,
       username: "Mj",
       name: "Michael Jordan",
       video:mjdunk,     
     },
     {
-      id: 8,
+      id: 5,
       username: "shaq",
       name: "Shaquille O'Neal",
       image:
         "https://i.discogs.com/t9FELUtAHyppVdYIkWQNUX5k35nD5LwNXfYMqwdMbA4/rs:fit/g:sm/q:90/h:742/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9BLTY2MTUz/LTE2MjkxNTExMzMt/NjM5MS5qcGVn.jpeg",
+    },
+    {
+      id: 6,
+      username: "alenIverson",
+      name: "Allen Iverson",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/2/21/Allen_Iverson_headshot.jpg",
+    },
+    {
+      id: 7,
+      username: "airCanada",
+      name: "Vince Carter",
+      reels:vincereels        
+    },
+    {
+      id: 8,
+      username: "tmac",
+      name: "Tracy Lamar McGrady",
+      image:
+        "https://media.gettyimages.com/id/51188472/photo/tracy-mcgrady-portraits.jpg?s=1024x1024&w=gi&k=20&c=V6RLb1Q3ZZrFGljovDMhhwJDcR1i_f_uDwqoZJ3x-6M=",
     },
     {
       id: 9,
-      username: "alenIverson",
-      name: "Allen Iverson",
-      image:
-        "https://upload.wikimedia.org/wikipedia/commons/2/21/Allen_Iverson_headshot.jpg",
+      username: "Mj",
+      name: "Michael Jordan",
+      video:mjdunk,     
     },
     {
       id: 10,
-      username: "airCanada",
-      name: "Vince Carter",
+      username: "shaq",
+      name: "Shaquille O'Neal",
       image:
-        "https://golfdigest.sports.sndimg.com/content/dam/images/golfdigest/fullset/2020/06/vc3.jpg.rend.hgtvcom.1280.914.suffix/1593191957820.jpeg",
+        "https://i.discogs.com/t9FELUtAHyppVdYIkWQNUX5k35nD5LwNXfYMqwdMbA4/rs:fit/g:sm/q:90/h:742/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9BLTY2MTUz/LTE2MjkxNTExMzMt/NjM5MS5qcGVn.jpeg",
     },
     {
       id: 11,
-      username: "tmac",
-      name: "Tracy Lamar McGrady",
+      username: "alenIverson",
+      name: "Allen Iverson",
       image:
-        "https://media.gettyimages.com/id/51188472/photo/tracy-mcgrady-portraits.jpg?s=1024x1024&w=gi&k=20&c=V6RLb1Q3ZZrFGljovDMhhwJDcR1i_f_uDwqoZJ3x-6M=",
+        "https://upload.wikimedia.org/wikipedia/commons/2/21/Allen_Iverson_headshot.jpg",
     },
     {
       id: 12,
-      username: "Mj",
-      name: "Michael Jordan",
-      video:mjdunk,
-      
+      username: "airCanada",
+      name: "Vince Carter",
+      reels:vincereels        
     },
     {
       id: 13,
-      username: "shaq",
-      name: "Shaquille O'Neal",
+      username: "tmac",
+      name: "Tracy Lamar McGrady",
       image:
-        "https://i.discogs.com/t9FELUtAHyppVdYIkWQNUX5k35nD5LwNXfYMqwdMbA4/rs:fit/g:sm/q:90/h:742/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9BLTY2MTUz/LTE2MjkxNTExMzMt/NjM5MS5qcGVn.jpeg",
+        "https://media.gettyimages.com/id/51188472/photo/tracy-mcgrady-portraits.jpg?s=1024x1024&w=gi&k=20&c=V6RLb1Q3ZZrFGljovDMhhwJDcR1i_f_uDwqoZJ3x-6M=",
     },
     {
       id: 14,
-      username: "alenIverson",
-      name: "Allen Iverson",
-      image:
-        "https://upload.wikimedia.org/wikipedia/commons/2/21/Allen_Iverson_headshot.jpg",
+      username: "Mj",
+      name: "Michael Jordan",
+      video:mjdunk,     
     },
     {
       id: 15,
-      username: "airCanada",
-      name: "Vince Carter",
-      image:
-        "https://golfdigest.sports.sndimg.com/content/dam/images/golfdigest/fullset/2020/06/vc3.jpg.rend.hgtvcom.1280.914.suffix/1593191957820.jpeg",
-    },
-    {
-      id: 16,
-      username: "tmac",
-      name: "Tracy Lamar McGrady",
-      image:
-        "https://media.gettyimages.com/id/51188472/photo/tracy-mcgrady-portraits.jpg?s=1024x1024&w=gi&k=20&c=V6RLb1Q3ZZrFGljovDMhhwJDcR1i_f_uDwqoZJ3x-6M=",
-    },
-    {
-      id: 12,
-      username: "Mj",
-      name: "Michael Jordan",
-      video:mjdunk,
-      
-    },
-    {
-      id: 18,
       username: "shaq",
       name: "Shaquille O'Neal",
       image:
         "https://i.discogs.com/t9FELUtAHyppVdYIkWQNUX5k35nD5LwNXfYMqwdMbA4/rs:fit/g:sm/q:90/h:742/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9BLTY2MTUz/LTE2MjkxNTExMzMt/NjM5MS5qcGVn.jpeg",
     },
-    {
-      id: 49,
-      username: "alenIverson",
-      name: "Allen Iverson",
-      image:
-        "https://upload.wikimedia.org/wikipedia/commons/2/21/Allen_Iverson_headshot.jpg",
-    },
-    {
-      id: 19,
-      username: "airCanada",
-      name: "Vince Carter",
-      image:
-        "https://golfdigest.sports.sndimg.com/content/dam/images/golfdigest/fullset/2020/06/vc3.jpg.rend.hgtvcom.1280.914.suffix/1593191957820.jpeg",
-    },
-    {
-      id: 20,
-      username: "tmac",
-      name: "Tracy Lamar McGrady",
-      image:
-        "https://media.gettyimages.com/id/51188472/photo/tracy-mcgrady-portraits.jpg?s=1024x1024&w=gi&k=20&c=V6RLb1Q3ZZrFGljovDMhhwJDcR1i_f_uDwqoZJ3x-6M=",
-    },
-    {
-      id: 12,
-      username: "Mj",
-      name: "Michael Jordan",
-      video:mjdunk,
-      
-    },
-    {
-      id: 22,
-      username: "shaq",
-      name: "Shaquille O'Neal",
-      image:
-        "https://i.discogs.com/t9FELUtAHyppVdYIkWQNUX5k35nD5LwNXfYMqwdMbA4/rs:fit/g:sm/q:90/h:742/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9BLTY2MTUz/LTE2MjkxNTExMzMt/NjM5MS5qcGVn.jpeg",
-    },
-    {
-      id: 23,
-      username: "alenIverson",
-      name: "Allen Iverson",
-      image:
-        "https://upload.wikimedia.org/wikipedia/commons/2/21/Allen_Iverson_headshot.jpg",
-    },
-    {
-      id: 24,
-      username: "airCanada",
-      name: "Vince Carter",
-      image:
-        "https://golfdigest.sports.sndimg.com/content/dam/images/golfdigest/fullset/2020/06/vc3.jpg.rend.hgtvcom.1280.914.suffix/1593191957820.jpeg",
-    },
-    {
-      id: 25,
-      username: "tmac",
-      name: "Tracy Lamar McGrady",
-      image:
-        "https://media.gettyimages.com/id/51188472/photo/tracy-mcgrady-portraits.jpg?s=1024x1024&w=gi&k=20&c=V6RLb1Q3ZZrFGljovDMhhwJDcR1i_f_uDwqoZJ3x-6M=",
-    },
-    {
-      id: 12,
-      username: "Mj",
-      name: "Michael Jordan",
-      video:mjdunk,
-      
-    },
-    {
-      id: 27,
-      username: "shaq",
-      name: "Shaquille O'Neal",
-      image:
-        "https://i.discogs.com/t9FELUtAHyppVdYIkWQNUX5k35nD5LwNXfYMqwdMbA4/rs:fit/g:sm/q:90/h:742/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9BLTY2MTUz/LTE2MjkxNTExMzMt/NjM5MS5qcGVn.jpeg",
-    },
+   
   ];
